e851ecd7d7e0480a45417f6863d8eb5641e46a51
gauravsahay007 <gauravsahay2468@gmail.com>
2024-07-02 06:42:12 +0530

    Fixed minor errors

--- my-app/app/api/myfiles/page.js
+++ my-app/app/api/myfiles/page.js
@@ -19,7 +19,7 @@ export default function Page() {
             <div className="grid grid-cols-1 md:grid-cols-3 w-full">
                 <div className="md:col-span-2">
                 <div className='bg-white mt-5 p-5 rounded-lg'>
-                    <h2 className='text-[18px] font-bold'>Recent Files</h2>
+                    <h2 className='text-[18px] font-bold'>My Files</h2>
                     <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 text-[13px] font-semibold border-b-[1px] pb-2 mt-3 border-gray-300 text-gray-400'>
                         <div className="col-span-2 lg:col-span-3">
                             <h2>Name</h2>

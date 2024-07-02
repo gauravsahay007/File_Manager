59d8934b76fc4ea1d04f81579de13829520617e1
gauravsahay007 <gauravsahay2468@gmail.com>
2024-07-02 06:35:17 +0530

    Fixed meta data

---
+++ my-app/app/api/myfiles/page.js
@@ -0,0 +1,46 @@
+"use client";
+import React,{useState,useEffect} from 'react';
+import SideNavBar from '@/app/components/SideNavBar';
+import { useData } from '@/context/DataProvider';
+import FolderItem from '@/app/components/Folder/FolderItem';
+import Storage from '@/app/components/Storage/Storage';
+import { useRouter } from 'next/navigation';
+export default function Page() {
+    const { state } = useData();
+    const router = useRouter();
+    const openFile = () => {
+        window.open(file.imageUrl, "_blank");
+    };
+    
+    return (
+        <div className="flex">
+            <SideNavBar />
+            <div className="grid grid-cols-1 md:grid-cols-3 w-full">
+                <div className="md:col-span-2">
+                <div className='bg-white mt-5 p-5 rounded-lg'>
+                    <h2 className='text-[18px] font-bold'>Recent Files</h2>
+                    <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 text-[13px] font-semibold border-b-[1px] pb-2 mt-3 border-gray-300 text-gray-400'>
+                        <div className="col-span-2 lg:col-span-3">
+                            <h2>Name</h2>
+                        </div>
+                        <div className="col-span-1 lg:col-span-1">
+                            <h2>Modified</h2>
+                        </div>
+                        <div className="col-span-1 lg:col-span-1">
+                            <h2>Size</h2>
+                        </div>
+                    </div>
+                    {state.fileList && state.fileList.map((item, index) => (
+                        <FileItem file={item} key={index} />
+                    ))}
+        </div>
+                </div>
+                <div className="md:col-span-1 flex justify-center items-center">
+                    <div className="flex flex-col items-center">
+                        <Storage />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
